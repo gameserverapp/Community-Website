f80b3b04963c3f9ab1f6f1d41f64f8057ee7d9ea
Max Vaessen <maxvaessen@gmail.com>
2019-12-16 16:07:03 +0100

    tweak git clone for public repo

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,8 +13,6 @@ fi
 
 apt update -y;
 
-echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==" >> ~/.ssh/known_hosts
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash;
 
 export NVM_DIR="$HOME/.nvm"
@@ -37,7 +35,7 @@ hash -r nvm
 apt install composer zip unzip php-mbstring php-dom -y;
 
 rm -rf /var/www/*;
-git clone git@github.com:GameserverApp/Community-Website.git /var/www;
+git clone https://github.com/GameserverApp/Community-Website.git /var/www;
 
 composer install -d /var/www;
 npm --prefix /var/www install;

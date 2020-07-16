ece79f7411852c135b142abce17076f965391301
Max Vaessen <maxvaessen@gmail.com>
2020-07-16 13:46:59 +0200

    big banner + aberration theme color fix + row loop tweak

--- resources/views/pages/v1/page/rows/row_loop.blade.php
+++ resources/views/pages/v1/page/rows/row_loop.blade.php
@@ -1,14 +1,38 @@
-<div class="row
-@if(
+<?php
+$classes = 'defaultcontent';
+
+if(
     isset($row['settings']['vertical_align']) and
     $row['settings']['vertical_align']
-)
-        display-table nowidth valign-{{$row['settings']['vertical_align']}}
-@endif
-    textcolor-{{$row['settings']['text_color'] or 'dark'}}
-    @if($rowTemplate == 'header' and (!isset($row['settings']['padding']) or $row['settings']['padding'] < 2)) padding-2 @else
-    padding-{{$row['settings']['padding'] or '2'}} @endif
-    defaultcontent">
+) {
+    $classes .= ' display-table nowidth valign-' . $row['settings']['vertical_align'];
+}
+
+if(isset($row['settings']['text_color'])) {
+    $classes .= ' textcolor-' . $row['settings']['text_color'];
+} else {
+    $classes .= ' textcolor-dark';
+}
+
+if(!isset($disable_padding)) {
+    if(
+        $rowTemplate == 'header' and
+        (
+            !isset($row['settings']['padding']) or
+            $row['settings']['padding'] < 2
+        )
+    ) {
+        $classes .= ' padding-2';
+    } elseif(isset($row['settings']['padding'])) {
+        $classes .= ' padding-' . $row['settings']['padding'];
+    } else {
+        $classes .= ' padding-2';
+    }
+}
+?>
+
+
+<div class="row {{$classes}}">
 
     @foreach($row['content'] as $key => $block)
         <div class="col-md-{{$block['size'] or 12}}

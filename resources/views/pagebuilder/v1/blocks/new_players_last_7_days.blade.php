13fe5574959d0f5313e9d494517477fc458388e5
Max Vaessen <maxvaessen@gmail.com>
2020-09-23 11:41:36 +0200

    graph blocks

--- resources/views/pagebuilder/v1/blocks/new_players_last_7_days.blade.php
+++ resources/views/pagebuilder/v1/blocks/new_players_last_7_days.blade.php
@@ -1,3 +1,6 @@
-@component('partials.v3.frame', ['title' => $block['title']])
+@component('partials.v3.frame', [
+    'title' => $block['title'],
+    'class' => 'tiny-padding'
+])
     <div class="stat_canvas" data-value="{{$value}}" data-route="{{route('stat.index', 'new-players-last-7-days')}}"><span>Loading...</span></div>
 @endcomponent
--- resources/views/pagebuilder/v1/blocks/online_count_last_7_days.blade.php
+++ resources/views/pagebuilder/v1/blocks/online_count_last_7_days.blade.php
@@ -1,3 +1,6 @@
-@component('partials.v3.frame', ['title' => $block['title']])
+@component('partials.v3.frame', [
+    'title' => $block['title'],
+    'class' => 'tiny-padding'
+])
     <div class="stat_canvas" data-value="{{$value}}" data-route="{{route('stat.index', 'online-count-last-7-days')}}"><span>Loading...</span></div>
 @endcomponent

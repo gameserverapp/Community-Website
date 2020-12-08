22f7eeb0cc652b1ef677a949d248742e206ffe8a
Max Vaessen <maxvaessen@gmail.com>
2020-12-08 15:48:31 +0100

    catch exception

--- app/Http/Middleware/CheckDomainSettings.php
+++ app/Http/Middleware/CheckDomainSettings.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Exceptions\DomainNotFoundException;
 use Closure;
 use GameserverApp\Api\Client;
 
@@ -18,7 +19,17 @@ class CheckDomainSettings
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        $domainSettings = Client::domain();
+        try {
+            $domainSettings = Client::domain();
+        } catch(DomainNotFoundException $e) {
+            $response = json_decode($e->getResponse()->getBody());
+
+            if(isset($response->redirect_url)) {
+                return redirect($response->redirect_url);
+            }
+
+            return redirect(config('gameserverapp.main_site'));
+        }
 
         if(
             $domainSettings instanceof \Exception and
@@ -32,7 +43,7 @@ class CheckDomainSettings
                 return redirect($response->redirect_url);
             }
 
-            return redirect('https://www.gameserverapp.com/');
+            return redirect(config('gameserverapp.main_site'));
         }
 
         return $next($request);

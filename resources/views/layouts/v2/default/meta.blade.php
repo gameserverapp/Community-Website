5e5c9b7545d0addaff843765482beaa4bbaf3db1
Max Vaessen <maxvaessen@gmail.com>
2023-05-27 13:13:38 +0200

    fix attempt weird GA

--- resources/views/layouts/v2/default/meta.blade.php
+++ resources/views/layouts/v2/default/meta.blade.php
@@ -60,19 +60,8 @@ $gaIds = GameserverApp\Helpers\SiteHelper::googleAnalyticsId();
     }
     ?>
 
-    @if(substr($firstId, 0, 2) == 'UA')
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    @if(substr($firstId, 0, 2) == 'G-')
 
-            ga('create', '{{$firstId}}', 'auto');
-    
-            ga('send', 'pageview');
-
-        </script>
-    @else
         <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id={{$firstId}}"></script>
         <script>
@@ -81,6 +70,18 @@ $gaIds = GameserverApp\Helpers\SiteHelper::googleAnalyticsId();
             gtag('js', new Date());
 
             gtag('config', '{{$firstId}}');
+        </script>
+    @else
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', '{{$firstId}}', 'auto');
+
+            ga('send', 'pageview');
+
         </script>
     @endif
 

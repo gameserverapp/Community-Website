869601641128e92f442c8e7ffecb6e975fbd6c5b
Max Vaessen <maxvaessen@gmail.com>
2025-08-19 12:35:36 +0200

    tweak form

--- resources/views/pagebuilder/v1/blocks/formbuilder.blade.php
+++ resources/views/pagebuilder/v1/blocks/formbuilder.blade.php
@@ -15,6 +15,7 @@ if(isset($block['title']) and !empty($block['title'])) {
 
 @component('partials.v3.frame', ['title' => $title, 'class' => 'formbuilder no-bottom-margin', 'footer' => $footer])
 
+
     @if(!auth()->check())
         <div class="alert alert-warning">
             Please <a href="{{route('auth.login')}}">login</a> to submit this form.<br>
@@ -22,37 +23,48 @@ if(isset($block['title']) and !empty($block['title'])) {
         </div>
     @elseif(
         isset($block['form_type']) and
-        $block['form_type'] == 'whitelist-application'
+        in_array($block['form_type'], [
+            'application',
+            'access-group'
+        ])
     )
+
         @if(
-            (
-                !auth()->user()->hasEmailSetup() or
-                !auth()->user()->emailConfirmed()
-            ) and
             isset($block['email_required']) and
             $block['email_required']
         )
-            <div class="alert alert-warning">
-                <i class="fa fa-exclamation-triangle" style="display:inline-block" aria-hidden="true"></i>
-                <span style="display:inline-block">
-                    You must have a confirmed <a href="{{route('user.settings', auth()->user()->id)}}">email address setup</a> to submit this form. <em><a href="{{route('user.settings', auth()->user()->id)}}">Privacy information</a></em>
-                </span>
-            </div>
-        @elseif(
-            auth()->user()->hasEmailSetup() and
-            auth()->user()->emailConfirmed()
-        )
-            <div class="alert alert-success">
-                Updates are sent to your email address.
-            </div>
+
+            @if(
+                auth()->user()->hasEmailSetup() and
+                auth()->user()->emailConfirmed()
+            )
+                <div class="alert alert-success">
+                    Updates are sent to your email address.
+                </div>
+            @else
+                <div class="alert alert-warning">
+                    <i class="fa fa-exclamation-triangle" style="display:inline-block" aria-hidden="true"></i>
+                    <span style="display:inline-block">
+                        You must have a confirmed <a href="{{route('user.settings', auth()->user()->id)}}">email address setup</a> to submit this form. <em><a href="{{route('user.settings', auth()->user()->id)}}">Privacy information</a></em>
+                    </span>
+                </div>
+
+            @endif
+
         @endif
 
-        @if(!auth()->user()->hasDiscordSetup())
+
+        @if(
+            isset($block['discord_required']) and
+            $block['discord_required'] and
+            !auth()->user()->hasDiscordSetup()
+        )
             <div class="alert alert-warning">
                 <i class="fa fa-exclamation-triangle" style="display:inline-block" aria-hidden="true"></i>
                 Please <a href="{{route('user.settings', auth()->user()->id)}}">connect your Discord</a> to continue.
             </div>
         @endif
+
     @endif
 
     @if(isset($block['description']) and !empty($block['description']))

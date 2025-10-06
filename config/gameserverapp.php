0353ea6af376017ef411626634824a968300477b
Max Vaessen <maxvaessen@gmail.com>
2025-10-06 16:23:39 +0200

    fix oauth base url

--- config/gameserverapp.php
+++ config/gameserverapp.php
@@ -7,7 +7,7 @@ return [
         'redirect' => env('GSA_REDIRECT_URL'),
         'timeout' => env('GSA_API_TIMEOUT', 20),
 
-        'oauth_base_url' => env('GSA_OAUTH_BASE_URL', 'https://player.gameserverapp.com/'),
+        'oauth_base_url' => env('GSA_OAUTH_BASE_URL', 'https://player.gameserverapp.com/api/v1/'),
     ],
 
     'main_site' => env('main_site', 'https://www.gameserverapp.com'),

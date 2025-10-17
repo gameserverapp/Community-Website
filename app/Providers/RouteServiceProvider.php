1e67a86fbceaf3112c3819d6b8d245e222b986ef
Max Vaessen <maxvaessen@gmail.com>
2025-10-17 15:22:08 +0200

    increase throttle limit to 120 calls per minute

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -36,7 +36,7 @@ class RouteServiceProvider extends ServiceProvider
     public function map()
     {
         Route::group([
-            'middleware' => 'throttle:60,1',
+            'middleware' => 'throttle:120,1',
         ], function ($r) {
             $this->mapWebRoutes();
 
